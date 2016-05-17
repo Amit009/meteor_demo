ceb360bf44063a7ae4289f28d914cb77c1832f9c
AMIT MISHRA <amitmishra@AMITs-Air.Home>
2016-05-17 17:53:03 +0530

    complete code

---
+++ client/recipes/RecipeSingle.js
@@ -0,0 +1,17 @@
+
+// Template level subscription
+Template.RecipeSingle.onCreated(function() {
+	var self = this;
+	self.autorun(function() {
+		var id = FlowRouter.getParam('id');
+		self.subscribe('singleRecipe', id);
+	});
+})
+
+
+Template.RecipeSingle.helpers({
+	recipe:function() {
+		var id = FlowRouter.getParam('id');
+		return Recipes.findOne({_id: id});
+	}
+})
--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,1 +1,16 @@
-Meteor.subscribe('recipes');
+// Meteor.subscribe('recipes');
+
+// Template level subscription
+Template.Recipes.onCreated(function() {
+	var self = this;
+	self.autorun(function() {
+		self.subscribe('recipes');
+	});
+})
+
+
+Template.Recipes.helpers({
+	recipes:function() {
+		return Recipes.find({});
+	}
+})
